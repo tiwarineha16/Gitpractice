430c45a25171f858e21fe2ac78f732ca82e8a5fa
Neha Tiwari <nehatiwaribt@gmail.com>
2023-07-22 11:23:12 +0530

    DeveloperA story

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-def add(a,b):
-    return a+b
-
-def multiply(a,b):
-    return a*b
-=======
 
 ## addition code
 def add(a,b,c):
+    print(a+b+c)
     return a+b+c
 ## multiplication code
 def multiply(a,b):
     return a*b
->>>>>>> be801e46a3349d6db9fed6d7b9029d4fbc32d9a7
+
+def divide(a,b):
+    return a/b
